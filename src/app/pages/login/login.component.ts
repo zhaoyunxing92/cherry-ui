74873d56ca5634ccfa2bc654fbea3432b613f2f5
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2020-06-08 11:52:27 +0800

    up:优化登陆表单

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,29 +12,28 @@ export class LoginComponent {
   constructor() {
   }
 
-  profileForm = new FormGroup({
+  /**
+   * 登陆表单验证
+   */
+  loginForm = new FormGroup({
     username: new FormControl('', [
       Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(16)
     ]),
-    bio: new FormControl('', [
+    password: new FormControl('', [
       Validators.required,
-      Validators.maxLength(256)
     ]),
-    phone: new FormControl('', Validators.required),
-    email: new FormControl('', [
-      Validators.required, Validators.email
-    ]),
-    option: new FormControl('', Validators.required)
   });
-  hide = true;
-  appearance = new FormControl();
-  isReadonly = new FormControl();
-  isDisabled = new FormControl();
-  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
-  onSubmit() {
-    console.warn(this.profileForm.value);
+  /**
+   * 是否显示密码
+   */
+  hidePassword = false;
+
+  /**
+   * 登陆
+   */
+  onLogin() {
+    console.log(this.loginForm.valid);
+    console.warn(this.loginForm.value);
   }
 }
