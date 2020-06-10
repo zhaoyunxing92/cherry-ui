1a85f071dc506c992d93003945f9cd1ca35fc568
zhaoyunxing92 <2385585770@qq.com>
2020-06-11 00:33:51 +0800

    add:

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,4 +1,23 @@
 import {Component, OnInit} from '@angular/core';
+import {LyTheme2} from '@alyle/ui';
+
+const styles = {
+  drawerContainer: {
+    height: '270px',
+    transform: 'translate3d(0,0,0)'
+  },
+  drawerContent: {
+    padding: '1em'
+  },
+  icon: {
+    margin: '0 8px'
+  },
+  grow: {
+    flexGrow: 1
+  }
+};
+const DEFAULT = '170px over@XSmall';
+const MINI = '56px over@XSmall';
 
 @Component({
   selector: 'app-index',
@@ -6,12 +25,19 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./index.component.less']
 })
 export class IndexComponent implements OnInit {
+  readonly classes = this.theme.addStyleSheet(styles);
+  mini = false;
+  get width() {
+    return this.mini ? MINI : DEFAULT;
+  }
 
-  constructor() {
+  constructor(private theme: LyTheme2) { }
+
+  toggleMini() {
+    this.mini = !this.mini;
   }
 
   ngOnInit(): void {
-
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,13 +3,18 @@ import {ShareModule} from '../share/share.module';
 import {NotFountComponent} from './not-fount/not-fount.component';
 import {LoginComponent} from './login/login.component';
 import {IndexComponent} from './index/index.component';
+import {LyToolbarModule} from '@alyle/ui/toolbar';
+import {LyDrawerModule} from '@alyle/ui/drawer';
+import {LyTypographyModule} from '@alyle/ui/typography';
+import {LyBadgeModule} from "@alyle/ui/badge";
+import {LyListModule} from "@alyle/ui/list";
 
 /**
  * 主要管理页面的模块
  */
 @NgModule({
   declarations: [LoginComponent, NotFountComponent, IndexComponent],
-  imports: [ShareModule],
+  imports: [ShareModule, LyToolbarModule, LyDrawerModule, LyTypographyModule, LyBadgeModule, LyListModule],
 })
 export class PagesModule {
 }
