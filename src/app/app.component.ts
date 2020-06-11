78be4320fce7c251579e3cbc4262e5a1fb48ffbd
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2020-06-11 10:52:16 +0800

    add:动态设置页面名称

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+
 import {lyl, StyleRenderer, ThemeVariables, WithStyles} from '@alyle/ui';
 
 const styles = (theme: ThemeVariables) => ({
@@ -28,9 +31,31 @@ const styles = (theme: ThemeVariables) => ({
 export class AppComponent implements OnInit, WithStyles {
   readonly classes = this.sRenderer.renderSheet(styles, true);
 
-  constructor(readonly sRenderer: StyleRenderer) {
+  constructor(readonly sRenderer: StyleRenderer, private router: Router, private title: Title) {
   }
 
+  private titleName = '';
+
   ngOnInit(): void {
+    this.router.events.pipe().subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.getTitle(this.router.routerState, this.router.routerState.root);
+        this.title.setTitle(this.titleName);
+      }
+    });
+  }
+
+  /**
+   * 获取路由上的data
+   * param: state
+   * param: parent
+   */
+  getTitle(state, parent): void {
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      this.titleName = parent.snapshot.data.title;
+    }
+    if (state && parent) {
+      this.getTitle(state, state.firstChild(parent));
+    }
   }
 }
