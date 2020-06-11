660acb1b081b4b5c2517405060671d5a4370d4fc
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2020-06-11 10:19:36 +0800

    up:module导入优化

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {HammerModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 // alyle-ui
 import {LyButtonModule} from '@alyle/ui/button';
 import {LyFieldModule} from '@alyle/ui/field';
@@ -10,6 +11,12 @@ import {LyIconModule} from '@alyle/ui/icon';
 import {LyCheckboxModule} from '@alyle/ui/checkbox';
 import {LyGridModule} from '@alyle/ui/grid';
 import {LyCardModule} from '@alyle/ui/card';
+import {LyToolbarModule} from '@alyle/ui/toolbar';
+import {LyAvatarModule} from '@alyle/ui/avatar';
+import {LyDrawerModule} from '@alyle/ui/drawer';
+import {LyTypographyModule} from '@alyle/ui/typography';
+import {LyBadgeModule} from '@alyle/ui/badge';
+import {LyListModule} from '@alyle/ui/list';
 
 /**
  * 共享模块,主要导入第三方ui库
@@ -27,6 +34,13 @@ import {LyCardModule} from '@alyle/ui/card';
     LyCheckboxModule,
     LyGridModule,
     LyCardModule,
+    LyToolbarModule,
+    LyAvatarModule,
+    LyToolbarModule,
+    LyDrawerModule,
+    LyTypographyModule,
+    LyBadgeModule,
+    LyListModule,
     HammerModule,
   ],
   exports: [
@@ -40,6 +54,13 @@ import {LyCardModule} from '@alyle/ui/card';
     LyCheckboxModule,
     LyGridModule,
     LyCardModule,
+    LyToolbarModule,
+    LyAvatarModule,
+    LyToolbarModule,
+    LyDrawerModule,
+    LyTypographyModule,
+    LyBadgeModule,
+    LyListModule,
     HammerModule,
   ]
 })
