6b473da4cb9e753f676236f108e20fe59cd12753
zhaoyunxing <wb-zyx735784@alibaba-inc.com>
2020-06-11 10:18:07 +0800

    up:修改首页样式

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -1,23 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {LyTheme2} from '@alyle/ui';
-
-const styles = {
-  drawerContainer: {
-    height: '270px',
-    transform: 'translate3d(0,0,0)'
-  },
-  drawerContent: {
-    padding: '1em'
-  },
-  icon: {
-    margin: '0 8px'
-  },
-  grow: {
-    flexGrow: 1
-  }
-};
-const DEFAULT = '170px over@XSmall';
-const MINI = '56px over@XSmall';
 
 @Component({
   selector: 'app-index',
@@ -25,19 +6,12 @@ const MINI = '56px over@XSmall';
   styleUrls: ['./index.component.less']
 })
 export class IndexComponent implements OnInit {
-  readonly classes = this.theme.addStyleSheet(styles);
-  mini = false;
-  get width() {
-    return this.mini ? MINI : DEFAULT;
-  }
 
-  constructor(private theme: LyTheme2) { }
-
-  toggleMini() {
-    this.mini = !this.mini;
+  constructor() {
   }
 
   ngOnInit(): void {
+
   }
 
 }
